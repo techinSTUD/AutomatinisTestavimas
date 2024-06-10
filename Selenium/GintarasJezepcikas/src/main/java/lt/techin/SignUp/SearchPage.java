66cc29c3ac53b7d7d96b99289f1ef0f9cc6bbd7f
Gintaras <test@example.com>
2024-06-10 14:36:49 +0300

    v2 su par

--- Selenium/GintarasJezepcikas/src/main/java/lt/techin/SignUp/SearchPage.java
+++ Selenium/GintarasJezepcikas/src/main/java/lt/techin/SignUp/SearchPage.java
@@ -1,9 +1,17 @@
 package lt.techin.SignUp;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SearchPage extends BasePage {
 
@@ -14,13 +22,37 @@ public class SearchPage extends BasePage {
     @FindBy(css = ".wishlist-button-product")
     private WebElement wishlistButton;
     @FindBy(css = ".modal-text")
-    private WebElement wishlistAlert;
+    private WebElement getWishlistAlert;
     @FindBy(css = ".show .btn-secondary")
     private WebElement cancelButton;
+
+    @FindBy(css = "[aria-label='Size']")
+    private WebElement sizeDropDown;
+    @FindBy(css = "h2 > a")
+    private WebElement getItemName;
 
 
     public SearchPage(WebDriver driver) {
         super(driver);
+    }
+
+    public static List<String> readCSV(String filePath) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static void main(String[] args) {
+        List<String> csvLines = readCSV("src/test/resources/products.csv");
+        System.out.println(csvLines);
     }
 
     void searchField(String ItemName) {
@@ -42,11 +74,32 @@ public class SearchPage extends BasePage {
     }
 
     String wishlistAlert() {
-       return wishlistAlert.getText();
+        return getWishlistAlert.getText();
     }
 
     void cancelButton() {
         cancelButton.click();
     }
 
+    void sizeDropDown() {
+        sizeDropDown.click();
+    }
+
+    String getItemName() {
+
+        return getItemName.getText();
+    }
+
+    public void selectSize(String size) {
+        WebElement sizeOption = driver.findElement(By.xpath("//option[text()='" + size + "']"));
+        sizeOption.click();
+    }
+
+    public String getSelectedSize() {
+        WebElement selectedSizeElement = driver.findElement(By.id("selected-size-id")); // Adjust selector as needed
+        return selectedSizeElement.getText();
+    }
+
+
 }
+
--- Selenium/GintarasJezepcikas/src/test/java/lt/techin/SignUp/SearchTest.java
+++ Selenium/GintarasJezepcikas/src/test/java/lt/techin/SignUp/SearchTest.java
@@ -1,39 +1,43 @@
 package lt.techin.SignUp;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static lt.techin.SignUp.SearchPage.readCSV;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SearchTest extends BaseTest {
 
 
-    @Test
-    void searchItems() {
-
-        String itemName = "HUMMINGBIRD PRINTED SWEATER";
-
+    @ParameterizedTest(name = "Find product and try to add to wishlist")
+    @CsvFileSource(resources = "/products.csv", numLinesToSkip = 1)
+    void pickSizeTest(String product, String price, String discount, String size) {
         SearchPage searchPage = new SearchPage(driver);
-        searchPage.searchField(itemName);
-        assertEquals(itemName, searchPage.itemName().toUpperCase());
+
+        searchPage.searchField(product);
+        assertEquals(product.toUpperCase(), searchPage.getItemName().toUpperCase());
         searchPage.clickItem();
-
-    }
-
-    @Test
-    void searchItemsAndAddToWishlist() {
-
-        String itemName = "HUMMINGBIRD PRINTED SWEATER";
-
-        SearchPage searchPage = new SearchPage(driver);
-        searchPage.searchField(itemName);
-        assertEquals(itemName, searchPage.itemName().toUpperCase());
-        searchPage.clickItem();
+        searchPage.selectSize(size);
         searchPage.wishlistButton();
         assertEquals("You need to be logged in to save " +
                 "products in your wishlist.", searchPage.wishlistAlert());
         searchPage.cancelButton();
 
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
